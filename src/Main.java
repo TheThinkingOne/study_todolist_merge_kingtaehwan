51941c0358a4e0916ab4059658661d5a627f8993
typhoon0678 <typhoon0678@gmail.com>
2024-08-08 10:38:27 +0900

    add output

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        // 결과값 받기
+        List<Map<String, Map<String, String>>> todoList = todoListList.output();
+        System.out.println("참여자별 ToDo list 진행 현황");
+
+        // 결과 출력
+        for (Map<String, Map<String, String>> userTodo : todoList) {
+            for (Map.Entry<String, Map<String, String>> userEntry : userTodo.entrySet()) {
+
+                System.out.printf("- %s\n", userEntry.getKey());
+
+                for (Map.Entry<String, String> todo : userEntry.getValue().entrySet()) {
+                    System.out.printf("%s(%s) ", todo.getKey(), todo.getValue());
+                }
+            }
+        }
+        System.out.print("완전 종료");
     }
 }
